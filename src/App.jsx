61effd9479dbcf766492f14125076b0b289aa485
philippe.legoff <philippe.legoff@cgi.com>
2023-10-04 10:29:31 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  document.getElementById('mainHeader').focus();
+  document.getElementById('focusDiv').focus();
 
   return (
     <>
